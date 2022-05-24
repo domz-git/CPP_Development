9dd91a6a30a637f0ddabed079064de7aa84a4e02
Dominik Filipovic <filjo2409@gmail.com>
2022-05-24 20:10:13 +0200

    variables in cpp

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    string name = "Peter";
+    int age = 40;
+    cout <<"My name is "<< name <<" and I'm "<< age <<" years old."<< endl;
+    
+    name = "Jack";
+    age = 24;
+    cout <<"My name is "<< name <<" and I'm "<< age <<" years old."<< endl;
+    return 0;
+
+}
