c1ce9900c8a70d4406c9627e16643465a7c8d99b
Dominik Filipovic <filjo2409@gmail.com>
2022-05-30 12:53:32 +0200

    finishing data types

---
+++ data_types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    char grade = 'A';
+    string phrase = "The grade is A";
+    int wholeNumber = 30;
+    float decimalNumber = 5.5;
+    double biggerDecimalNumber = 6.555;
+    bool state = true;
+    
+    cout <<grade<< endl;
+    cout <<phrase<< endl;
+    cout <<wholeNumber<< endl;
+    cout <<decimalNumber<< endl;
+    cout <<biggerDecimalNumber<< endl;
+    cout <<state<< endl;
+
+
+    return 0;
+
+}
