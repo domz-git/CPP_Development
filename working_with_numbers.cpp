0bb363f8c63c1eed57d7cfc5ff636d3416265333
Dominik Filipovic <filjo2409@gmail.com>
2022-05-31 10:46:08 +0200

    Brief overview of number in cpp

---
+++ working_with_numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+
+    cout<<"Two to the power of five is: "<<pow(2,5)<<endl;
+    cout<<"The square root of 36 is: "<<sqrt(36)<<endl;
+    cout<<"The rounded value of 3.14 is: "<<round(3.14)<<endl;
+    cout<<"The highest rounded value of 3.14 is: "<<ceil(3.14)<<endl;
+    cout<<"The lowest rounded value of 3.14 is: "<<floor(3.14)<<endl;
+    cout<<"Between 5 and 10, the bigger number is: "<<fmax(5, 10)<<endl;
+    cout<<"Between 6 and 12, the smaller number is: "<<fmin(6,12)<<endl;
+
+return 0;
+
+}
