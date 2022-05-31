929bb68ac955d84840d6b0e3714d8bf96a90eaec
Dominik Filipovic <filjo2409@gmail.com>
2022-05-31 11:02:30 +0200

    basics of user inpput

---
+++ user_input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int value;
+    string someString;
+    cout<<"Enter a number: ";
+    cin>>value;
+    cout<<"You've entered: "<<value<<endl;
+    cout<<"Enter a string: ";
+    cin>>someString;
+    cout<<"You've entered: "<<someString<<endl;
+
+return 0;
+
+}
